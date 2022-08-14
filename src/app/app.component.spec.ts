c6f8b0fbc125a63dd6ba1c4c67a721a0531683e1
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2022-08-13 22:42:26 -0400

    Enable the other spec files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,16 +22,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  // it(`should have as title 'ez-recipes'`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('ez-recipes');
-  // });
+  it(`shouldn't show a recipe initially`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.recipe).toBeUndefined();
+  });
 
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('.content span')?.textContent).toContain('ez-recipes app is running!');
-  // });
+  it('should render the find recipe button', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.textContent).toContain('Find me a recipe!');
+  });
 });
