2294bef5c883fd466e63de52e981f972abba3455
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2022-08-22 13:17:12 -0400

    Add more tests to AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,28 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  fakeAsync,
+  TestBed,
+  tick,
+} from '@angular/core/testing';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { mockRecipe } from './models/recipe.mock';
 import { NavbarComponent } from './navbar/navbar.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let appComponent: AppComponent;
+  let rootElement: HTMLElement;
+
   beforeEach(async () => {
+    // Import all the necessary modules and components to test the app component
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
@@ -21,24 +34,67 @@ describe('AppComponent', () => {
       ],
       declarations: [AppComponent, NavbarComponent],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges(); // re-render the component
+    appComponent = fixture.componentInstance;
+    rootElement = fixture.nativeElement;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    // Check that the component can render
+    expect(appComponent).toBeTruthy();
   });
 
   it(`shouldn't show a recipe initially`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.recipe).toBeUndefined();
+    // Check that the page initially shows the "Find me a recipe!" button
+    const findRecipeButton = rootElement.querySelector<HTMLButtonElement>(
+      '.find-recipe-button'
+    );
+    expect(findRecipeButton).not.toBeNull();
+    expect(findRecipeButton?.textContent).toContain('Find me a recipe!');
+
+    // The navbar should be visible
+    expect(
+      fixture.debugElement.query(By.directive(NavbarComponent))
+    ).toBeDefined();
+
+    // The spinner should be hidden
+    expect(appComponent.isLoading).toBeFalse();
+    expect(rootElement.querySelector('.progress-spinner')).toBeNull();
+
+    // No recipe should appear
+    expect(appComponent.recipe).toBeUndefined();
   });
 
-  it('should render the find recipe button', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it('should load a random recipe after clicking the find recipe button', fakeAsync(() => {
+    // Check that the getRandomRecipe method is called after clicking the find recipe button
+    spyOn(appComponent, 'getRandomRecipe');
+
+    const findRecipeButton = rootElement.querySelector<HTMLButtonElement>(
+      '.find-recipe-button'
+    );
+    expect(findRecipeButton).not.toBeNull();
+    findRecipeButton?.click();
+    tick(); // wait for the async tasks to complete
+
+    expect(appComponent.getRandomRecipe).toHaveBeenCalled();
+  }));
+
+  it('should show a spinner while loading', () => {
+    // Check that the material spinner shows when isLoading is true
+    appComponent.isLoading = true;
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.textContent).toContain('Find me a recipe!');
+    expect(rootElement.querySelector('.progress-spinner')).not.toBeNull();
+  });
+
+  it('should show the recipe component if recipe is defined', () => {
+    // Check that the app recipe component appears after loading a recipe
+    appComponent.recipe = mockRecipe;
+    fixture.detectChanges();
+    // debugElement allows for more Angular-specific queries
+    expect(
+      fixture.debugElement.query(By.directive(RecipeComponent))
+    ).toBeDefined();
   });
 });
