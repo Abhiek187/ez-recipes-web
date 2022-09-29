4b2e029d9732f7b197623dd0905f6145c9215541
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2022-09-28 21:12:00 -0400

    Toggle the heart icon when clicked

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  private _isFavorite = false;
+  heartIcon = 'favorite_border';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get isFavorite(): boolean {
+    return this._isFavorite;
   }
 
+  set isFavorite(value: boolean) {
+    // Update the material icon to match the corresponding boolean value
+    this._isFavorite = value;
+    this.heartIcon = this._isFavorite ? 'favorite' : 'favorite_border';
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  toggleFavoriteRecipe() {
+    // Placeholder for the heart button
+    this.isFavorite = !this.isFavorite;
+  }
 }
