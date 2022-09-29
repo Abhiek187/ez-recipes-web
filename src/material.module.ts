66ce03853e7f07872c52c5dddec17cd4db2cc65c
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2022-09-28 22:34:57 -0400

    Move material imports to their own module

---
+++ src/material.module.ts
@@ -0,0 +1,26 @@
+// Holds all imports for Angular Material
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  exports: [
+    MatButtonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatIconModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatSidenavModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+  ],
+})
+export class MaterialModule {}
