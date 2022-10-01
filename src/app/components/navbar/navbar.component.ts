685c398a3da7cb76b6ab56bec5c56b7e1a6909c4
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2022-10-01 19:23:32 -0400

    Hide the favorite and share buttons if a recipe isn't shown

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,15 +2,18 @@ import { BreakpointObserver } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 import { Router, Routes } from '@angular/router';
 
+import { RecipeComponent } from '../recipe/recipe.component';
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  isRecipePage = false;
   isFavorite = false;
   isSmallScreen: boolean;
-  navItems: string[] = ['Home'];
+  navItems: string[] = ['Home']; // navigation links to show in the sidenav
   routerConfig: Routes;
 
   constructor(
@@ -24,6 +27,11 @@ export class NavbarComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  onRouterOutletActivate(event: any) {
+    // Check if the recipe component is shown in the router outlet
+    this.isRecipePage = event instanceof RecipeComponent;
+  }
 
   toggleFavoriteRecipe() {
     // Placeholder for the heart button
