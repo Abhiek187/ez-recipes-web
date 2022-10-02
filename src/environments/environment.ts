ce211e4b14b9fabd6faa759cb9c8bc7f2fc12454
Abhishek Chaudhuri <abhishek.chaudhuri@prudential.com>
2022-10-02 14:25:37 -0400

    Revert mock=true

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   recipeBaseUrl: 'http://localhost:5000/api/recipes',
-  mock: true, // only enable for debugging, tests will fail if this is left on
+  mock: false, // only enable for debugging, tests will fail if this is left on
 };
 
 /*
