210400cbb3680817c90ef5f43110771cebff47e9
Abhiek187 <achaudhuri2011@yahoo.com>
2024-03-24 14:53:29 -0400

    Switch mock back to false

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   serverBaseUrl: 'http://localhost:5000',
-  mock: true, // only enable for debugging, tests will fail if this is left on
+  mock: false, // only enable for debugging, tests will fail if this is left on
 };
 
 /*
