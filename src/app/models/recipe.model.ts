a3fc3f9a22244b24ede6e99c278c883e2445f9ec
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2024-03-23 22:35:46 -0400

    Update recipe mocks & model

--- src/app/models/recipe.model.ts
+++ src/app/models/recipe.model.ts
@@ -1,14 +1,103 @@
+export const SPICE_LEVELS = ['none', 'mild', 'spicy', 'unknown'] as const;
+export type SpiceLevel = (typeof SPICE_LEVELS)[number];
+export const isValidSpiceLevel = (
+  str: string
+): str is Exclude<SpiceLevel, 'unknown'> => {
+  return SPICE_LEVELS.includes(str as SpiceLevel) && str !== 'unknown';
+};
+
+export const MEAL_TYPES = [
+  'main course',
+  'side dish',
+  'dessert',
+  'appetizer',
+  'salad',
+  'bread',
+  'breakfast',
+  'soup',
+  'beverage',
+  'sauce',
+  'marinade',
+  'fingerfood',
+  'snack',
+  'drink',
+  'antipasti',
+  'starter',
+  'antipasto',
+  "hor d'oeuvre",
+  'lunch',
+  'main dish',
+  'dinner',
+  'morning meal',
+  'brunch',
+  'condiment',
+  'dip',
+  'spread',
+] as const;
+export type MealType = (typeof MEAL_TYPES)[number];
+export const isValidMealType = (str: string): str is MealType => {
+  return MEAL_TYPES.includes(str as MealType);
+};
+
+export const CUISINES = [
+  'African',
+  'Asian',
+  'American',
+  'British',
+  'Cajun',
+  'Caribbean',
+  'Chinese',
+  'Eastern European',
+  'European',
+  'French',
+  'German',
+  'Greek',
+  'Indian',
+  'Irish',
+  'Italian',
+  'Japanese',
+  'Jewish',
+  'Korean',
+  'Latin American',
+  'Mediterranean',
+  'Mexican',
+  'Middle Eastern',
+  'Nordic',
+  'Southern',
+  'Spanish',
+  'Thai',
+  'Vietnamese',
+  'English',
+  'Scottish',
+  'South American',
+  'Creole',
+] as const;
+export type Cuisine = (typeof CUISINES)[number];
+export const isValidCuisine = (str: string): str is Cuisine => {
+  return CUISINES.includes(str as Cuisine);
+};
+
 type Recipe = {
   id: number;
   name: string;
   url: string;
   image: string;
-  credit: string | null;
+  credit: string;
   sourceUrl: string;
   healthScore: number;
   time: number;
   servings: number;
   summary: string;
+  types: MealType[];
+  spiceLevel: SpiceLevel;
+  isVegetarian: boolean;
+  isVegan: boolean;
+  isGlutenFree: boolean;
+  isHealthy: boolean;
+  isCheap: boolean;
+  isSustainable: boolean;
+  culture: Cuisine[];
+  //allergies: string[];
   nutrients: {
     name: string;
     amount: number;
