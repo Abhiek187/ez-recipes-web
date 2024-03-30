c2f3f5698d1b2b35e42c07939c67bf372d10a64d
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2024-03-30 17:08:08 -0400

    Remove extra fluff

--- src/app/components/page-not-found/page-not-found.component.ts
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -8,8 +8,4 @@ import { RouterLink } from '@angular/router';
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss'],
 })
-export class PageNotFoundComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class PageNotFoundComponent {}
