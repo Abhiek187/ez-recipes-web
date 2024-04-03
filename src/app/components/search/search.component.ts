8466c17b506fc7750313403868d6ea1b4005a512
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2024-04-02 20:53:38 -0400

    Add form errors

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import {
+  AbstractControl,
   FormControl,
   FormGroup,
   ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
   Validators,
 } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
@@ -31,6 +34,20 @@ type PartialNull<T> = {
   [P in keyof T]?: T[P] | null;
 };
 
+// Check that minCals doesn't exceed maxCals
+const calorieRangeValidator: ValidatorFn = (
+  control: AbstractControl
+): ValidationErrors | null => {
+  const minCals = control.get('minCals');
+  const maxCals = control.get('maxCals');
+
+  return minCals?.value !== null &&
+    maxCals?.value !== null &&
+    minCals?.value > maxCals?.value
+    ? { range: true }
+    : null;
+};
+
 @Component({
   selector: 'app-search',
   standalone: true,
@@ -50,30 +67,40 @@ type PartialNull<T> = {
   styleUrl: './search.component.scss',
 })
 export class SearchComponent {
-  filterFormGroup = new FormGroup({
-    query: new FormControl(''),
-    minCals: new FormControl<number | null>(null, [
-      Validators.min(0),
-      Validators.max(2000),
-    ]),
-    maxCals: new FormControl<number | null>(null, [
-      Validators.min(0),
-      Validators.max(2000),
-    ]),
-    vegetarian: new FormControl(false),
-    vegan: new FormControl(false),
-    glutenFree: new FormControl(false),
-    healthy: new FormControl(false),
-    cheap: new FormControl(false),
-    sustainable: new FormControl(false),
-    spiceLevel: new FormControl([]),
-    type: new FormControl([]),
-    culture: new FormControl([]),
-  });
+  filterFormGroup = new FormGroup(
+    {
+      query: new FormControl(''),
+      minCals: new FormControl<number | null>(null, [
+        Validators.min(0),
+        Validators.max(2000),
+      ]),
+      maxCals: new FormControl<number | null>(null, [
+        Validators.min(0),
+        Validators.max(2000),
+      ]),
+      vegetarian: new FormControl(false),
+      vegan: new FormControl(false),
+      glutenFree: new FormControl(false),
+      healthy: new FormControl(false),
+      cheap: new FormControl(false),
+      sustainable: new FormControl(false),
+      spiceLevel: new FormControl([]),
+      type: new FormControl([]),
+      culture: new FormControl([]),
+    },
+    { validators: calorieRangeValidator }
+  );
+  readonly Errors = {
+    min: 'Calories must be ≥ 0',
+    max: 'Calories must be ≤ 2000',
+    range: 'Max calories cannot exceed min calories',
+    noResults: 'No recipes found',
+  };
 
   isLoading = false;
   private defaultLoadingMessage = '';
   loadingMessage = this.defaultLoadingMessage;
+  noRecipesFound = false;
 
   // Exclude unknown cases and sort for ease of reference
   readonly spiceLevels = SPICE_LEVELS.filter(
@@ -98,6 +125,7 @@ export class SearchComponent {
         this.isLoading = false;
         clearInterval(timer);
         console.log('Found recipes:', recipes);
+        this.noRecipesFound = recipes.length === 0;
       },
       error: (error: Error) => {
         this.isLoading = false;
