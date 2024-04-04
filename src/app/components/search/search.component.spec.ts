8723bc78eb9786128706916fb538e2c8b2642730
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2024-04-03 22:40:44 -0400

    Start writing tests for the filter form

--- src/app/components/search/search.component.spec.ts
+++ src/app/components/search/search.component.spec.ts
@@ -1,12 +1,18 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ComponentFixture,
+  TestBed,
+  fakeAsync,
+  tick,
+} from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { SearchComponent } from './search.component';
 
 describe('SearchComponent', () => {
-  let component: SearchComponent;
+  let searchComponent: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
+  let rootElement: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,11 +20,41 @@ describe('SearchComponent', () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(SearchComponent);
-    component = fixture.componentInstance;
     fixture.detectChanges();
+    searchComponent = fixture.componentInstance;
+    rootElement = fixture.nativeElement;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should show the filter form', () => {
+    // Check that all the form elements are present
+    expect(searchComponent).toBeTruthy();
+  });
+
+  it('should load recipes after submitting the initial form', fakeAsync(() => {
+    // By default, the form should be valid
+    spyOn(searchComponent, 'onSubmit');
+
+    const submitButton =
+      rootElement.querySelector<HTMLButtonElement>('.submit-button');
+    expect(submitButton).not.toBeNull();
+    expect(submitButton?.disabled).toBeFalse();
+    submitButton?.click();
+    tick();
+
+    expect(searchComponent.onSubmit).toHaveBeenCalled();
+  }));
+
+  it('should remove null filter values', () => {
+    // Check that removeNullValues() removes null values from a recipe filter
+    const filter = {
+      query: 'cake',
+      minCals: null,
+      maxCals: 1000,
+    };
+    const nonNullFilter = searchComponent.removeNullValues(filter);
+    expect(nonNullFilter).toEqual({
+      query: filter.query,
+      maxCals: filter.maxCals,
+    });
   });
 });
