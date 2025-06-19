38a62ff4160455ade81c86c8c628f5b8bd5c9d26
Abhishek Chaudhuri <achaudhuri2011@yahoo.com>
2025-06-19 08:45:45 -0400

    Create the update password form

--- src/app/components/profile/update-password/update-password.component.spec.ts
+++ src/app/components/profile/update-password/update-password.component.spec.ts
@@ -1,23 +1,173 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { UpdatePasswordComponent } from './update-password.component';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+
+import { mockChef, mockChefEmailResponse } from 'src/app/models/profile.mock';
+import { ChefUpdateType } from 'src/app/models/profile.model';
+import { ChefService } from 'src/app/services/chef.service';
+import { profileRoutes } from 'src/app/app-routing.module';
 
 describe('UpdatePasswordComponent', () => {
-  let component: UpdatePasswordComponent;
+  let updatePasswordComponent: UpdatePasswordComponent;
   let fixture: ComponentFixture<UpdatePasswordComponent>;
+  let rootElement: HTMLElement;
+  let router: Router;
+  let mockChefService: jasmine.SpyObj<ChefService>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [UpdatePasswordComponent]
-    })
-    .compileComponents();
+    mockChefService = jasmine.createSpyObj('ChefService', [
+      'getChef',
+      'updateChef',
+    ]);
+    mockChefService.getChef.and.returnValue(of(mockChef));
 
+    await TestBed.configureTestingModule({
+      imports: [UpdatePasswordComponent],
+      providers: [
+        provideHttpClient(withInterceptorsFromDi()),
+        provideHttpClientTesting(),
+        {
+          provide: ChefService,
+          useValue: mockChefService,
+        },
+      ],
+    }).compileComponents();
+
+    const localStorageProto = Object.getPrototypeOf(localStorage);
+    spyOn(localStorageProto, 'getItem').and.returnValue(mockChef.token);
+    spyOn(localStorageProto, 'setItem').and.callFake(() => {});
+    spyOn(localStorageProto, 'removeItem').and.callFake(() => {});
+
+    router = TestBed.inject(Router);
     fixture = TestBed.createComponent(UpdatePasswordComponent);
-    component = fixture.componentInstance;
+    updatePasswordComponent = fixture.componentInstance;
+    rootElement = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(updatePasswordComponent).toBeTruthy();
+    expect(rootElement.textContent).toContain('Change Password');
+    expect(rootElement.textContent).toContain('New Password');
+    expect(rootElement.textContent).toContain('Confirm Password');
+    expect(rootElement.textContent).toContain(
+      'Password must be at least 8 characters long'
+    );
+    expect(rootElement.textContent).toContain('Submit');
+
+    const signUpFields = rootElement.querySelector('.password-fields');
+    const [passwordField, confirmPasswordField] = Array.from(
+      signUpFields?.querySelectorAll<HTMLInputElement>('input') ?? []
+    );
+
+    expect(passwordField.type).toBe('password');
+    expect(passwordField.autocapitalize).toBe('none');
+    expect(passwordField.autocomplete).toBe('off');
+    expect(passwordField.spellcheck).toBeFalse();
+
+    expect(confirmPasswordField.type).toBe('password');
+    expect(confirmPasswordField.autocapitalize).toBe('none');
+    expect(confirmPasswordField.autocomplete).toBe('off');
+    expect(confirmPasswordField.spellcheck).toBeFalse();
+
+    updatePasswordComponent.showPassword.set(true);
+    fixture.detectChanges();
+    expect(passwordField.type).toBe('text');
+    updatePasswordComponent.showPassword.set(false);
+    fixture.detectChanges();
+    expect(passwordField.type).toBe('password');
+
+    updatePasswordComponent.showPasswordConfirm.set(true);
+    fixture.detectChanges();
+    expect(confirmPasswordField.type).toBe('text');
+    updatePasswordComponent.showPasswordConfirm.set(false);
+    fixture.detectChanges();
+    expect(confirmPasswordField.type).toBe('password');
+  });
+
+  it("should show an error if the password isn't provided", () => {
+    const form = updatePasswordComponent.formGroup;
+    form.controls.password.setValue(null);
+    fixture.detectChanges();
+
+    expect(form.valid).toBeFalse();
+    expect(
+      form.controls.password.hasError(
+        updatePasswordComponent.formErrors.required
+      )
+    ).toBeTrue();
+
+    const submitButton = rootElement
+      .querySelector('.submit-row')
+      ?.querySelector('button');
+    expect(submitButton?.disabled).toBeTrue();
+  });
+
+  it('should show an error if the password is too short', () => {
+    const form = updatePasswordComponent.formGroup;
+    form.controls.password.setValue('123');
+    fixture.detectChanges();
+
+    expect(form.valid).toBeFalse();
+    expect(
+      form.controls.password.hasError(
+        updatePasswordComponent.formErrors.passwordMinLength
+      )
+    ).toBeTrue();
+    const submitButton = rootElement
+      .querySelector('.submit-row')
+      ?.querySelector('button');
+    expect(submitButton?.disabled).toBeTrue();
+  });
+
+  it("should show an error if the passwords don't match", () => {
+    const form = updatePasswordComponent.formGroup;
+    form.controls.password.setValue('password1');
+    form.controls.passwordConfirm.setValue('password2');
+    fixture.detectChanges();
+
+    expect(form.valid).toBeFalse();
+    expect(
+      form.hasError(updatePasswordComponent.formErrors.passwordMismatch)
+    ).toBeTrue();
+    const submitButton = rootElement
+      .querySelector('.submit-row')
+      ?.querySelector('button');
+    expect(submitButton?.disabled).toBeTrue();
+  });
+
+  it('should enable the submit button if the email is valid', () => {
+    const form = updatePasswordComponent.formGroup;
+    const mockPassword = 'password123';
+    form.controls.password.setValue(mockPassword);
+    form.controls.passwordConfirm.setValue(mockPassword);
+    fixture.detectChanges();
+
+    expect(form.valid).toBeTrue();
+    const submitButton = rootElement
+      .querySelector('.submit-row')
+      ?.querySelector('button');
+    expect(submitButton?.disabled).toBeFalse();
+
+    mockChefService.updateChef.and.returnValue(of(mockChefEmailResponse));
+    const navigateSpy = spyOn(router, 'navigate');
+    submitButton?.click();
+    fixture.detectChanges();
+
+    expect(mockChefService.updateChef).toHaveBeenCalledWith(
+      {
+        type: ChefUpdateType.Password,
+        email: mockChef.email,
+        password: mockPassword,
+      },
+      mockChef.token
+    );
+    expect(navigateSpy).toHaveBeenCalledWith([profileRoutes.login.path]);
   });
 });
--- src/app/components/profile/update-password/update-password.component.ts
+++ src/app/components/profile/update-password/update-password.component.ts
@@ -1,11 +1,154 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  ValidatorFn,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+import Constants from 'src/app/constants/constants';
+import { ChefUpdate, ChefUpdateType } from 'src/app/models/profile.model';
+import { ChefService } from 'src/app/services/chef.service';
+import { profileRoutes } from 'src/app/app-routing.module';
+
+const formControls = {
+  password: 'password',
+  passwordConfirm: 'passwordConfirm',
+} as const;
+const formErrors = {
+  required: 'required',
+  passwordMinLength: 'minlength',
+  passwordMismatch: 'passwordMismatch',
+} as const;
+
+const passwordsMatchValidator: ValidatorFn = (control) => {
+  const password = control.get(formControls.password);
+  const passwordConfirm = control.get(formControls.passwordConfirm);
+
+  const error =
+    password?.value !== null &&
+    passwordConfirm?.value !== null &&
+    password?.value !== passwordConfirm?.value
+      ? {
+          [formErrors.passwordMismatch]: true,
+        }
+      : null;
+  passwordConfirm?.setErrors(error);
+  return error;
+};
 
 @Component({
   selector: 'app-update-password',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './update-password.component.html',
-  styleUrl: './update-password.component.scss'
+  styleUrl: './update-password.component.scss',
 })
-export class UpdatePasswordComponent {
+export class UpdatePasswordComponent implements OnInit, OnDestroy {
+  private chefService = inject(ChefService);
+  private snackBar = inject(MatSnackBar);
+  private router = inject(Router);
 
+  private chefServiceSubscription?: Subscription;
+
+  showPassword = signal(false);
+  showPasswordConfirm = signal(false);
+  isLoading = signal(false);
+  private chefEmail = signal('');
+
+  formControls = formControls;
+  formErrors = formErrors;
+  formGroup = new FormGroup(
+    {
+      [formControls.password]: new FormControl('', [
+        Validators.required,
+        Validators.minLength(Constants.passwordMinLength),
+      ]),
+      [formControls.passwordConfirm]: new FormControl('', [
+        Validators.required,
+      ]),
+    },
+    { validators: passwordsMatchValidator }
+  );
+  readonly errors = {
+    [formErrors.required]: 'Error: password is required',
+    [formErrors.passwordMinLength]: `Error: Password must be at least ${Constants.passwordMinLength} characters long`,
+    [formErrors.passwordMismatch]: 'Error: Passwords do not match',
+  } as const;
+
+  ngOnInit(): void {
+    const token = localStorage.getItem(Constants.LocalStorage.token);
+    if (token === null) return;
+
+    this.chefService.getChef(token).subscribe({
+      next: ({ email }) => {
+        this.chefEmail.set(email);
+      },
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.chefServiceSubscription?.unsubscribe();
+  }
+
+  togglePasswordVisibility(event: MouseEvent) {
+    event.preventDefault();
+    this.showPassword.set(!this.showPassword());
+  }
+
+  togglePasswordConfirmVisibility(event: MouseEvent) {
+    event.preventDefault();
+    this.showPasswordConfirm.set(!this.showPasswordConfirm());
+  }
+
+  updatePassword() {
+    this.isLoading.set(true);
+    const { password } = this.formGroup.value;
+    const fields: ChefUpdate = {
+      type: ChefUpdateType.Password,
+      email: this.chefEmail(),
+      password: password ?? '',
+    };
+    const token = localStorage.getItem(Constants.LocalStorage.token);
+    if (token === null) {
+      this.isLoading.set(false);
+      this.snackBar.open(Constants.noTokenFound, 'Dismiss');
+      return;
+    }
+
+    this.chefServiceSubscription = this.chefService
+      .updateChef(fields, token)
+      .subscribe({
+        next: () => {
+          this.isLoading.set(false);
+
+          // The token will be revoked, so sign out the user
+          localStorage.removeItem(Constants.LocalStorage.token);
+          this.snackBar.open(
+            'Password updated successfully! Please sign in again.',
+            'Dismiss'
+          );
+          this.router.navigate([profileRoutes.login.path]);
+        },
+        error: (error) => {
+          this.isLoading.set(false);
+          this.snackBar.open(error.message, 'Dismiss');
+        },
+      });
+  }
 }
