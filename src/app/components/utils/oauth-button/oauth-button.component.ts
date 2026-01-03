c09587469439538aacd5c404b7a2d0b0c1919a0b
Abhiek187 <achaudhuri2011@yahoo.com>
2026-01-02 21:45:36 -0500

    Fix the styling of the OAuth buttons

--- src/app/components/utils/oauth-button/oauth-button.component.ts
+++ src/app/components/utils/oauth-button/oauth-button.component.ts
@@ -1,22 +1,38 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, inject, input, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 import Constants from 'src/app/constants/constants';
 import { Provider } from 'src/app/models/profile.model';
 
 @Component({
   selector: 'app-oauth-button',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './oauth-button.component.html',
   styleUrl: './oauth-button.component.scss',
 })
-export class OauthButtonComponent {
+export class OauthButtonComponent implements OnInit {
+  private iconRegistry = inject(MatIconRegistry);
+  private sanitizer = inject(DomSanitizer);
+
   readonly provider = input.required<Provider>();
   readonly authUrl = input<string>();
 
   readonly providerStyle = computed(
     () => Constants.providerStyles[this.provider()]
   );
+  readonly providerIcon = computed(() => this.providerStyle().label);
+
+  ngOnInit(): void {
+    // Add all the SVGs inline so they can be stylized
+    this.iconRegistry.addSvgIcon(
+      this.providerIcon(),
+      this.sanitizer.bypassSecurityTrustResourceUrl(
+        `assets/${this.providerStyle().icon}`
+      )
+    );
+  }
 
   startOAuthFlow(event: MouseEvent) {
     event.preventDefault();
