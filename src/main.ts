b4c136a256a2e7febcf9604b2e6c3d0a49464094
Abhiek187 <achaudhuri2011@yahoo.com>
2026-06-12 23:20:56 -0400

    Switch to zoneless change detection

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import {
 import {
   enableProdMode,
   importProvidersFrom,
-  provideZoneChangeDetection,
+  provideZonelessChangeDetection,
 } from '@angular/core';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { ServiceWorkerModule } from '@angular/service-worker';
@@ -20,7 +20,7 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideZoneChangeDetection(),
+    provideZonelessChangeDetection(),
     importProvidersFrom(
       BrowserModule,
       AppRoutingModule,
